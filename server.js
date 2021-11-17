be7d0bebcbbde26cf60c310538479c703ffcf2eb
Anthony <ajsur1017@gmail.com>
2021-11-16 19:41:30 -0500

    completed backend

---
+++ server.js
@@ -0,0 +1,93 @@
+////
+// Dependencies
+////
+require("dotenv").config()
+const { PORT, MONGODB_URL } = process.env
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const cors = require("cors")
+const morgan = require("morgan")
+
+////
+// Connection
+////
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+// Connection Events
+mongoose.connection
+.on("open", () => console.log("Connected to Mongo"))
+.on("close", () => console.log("Disonnected to Mongo"))
+.on("error", (error) => console.log(error))
+
+////
+// Models
+////
+const EventsSchema = new mongoose.Schema({
+    name: String,
+    dateDay: Number,
+    dateMonth: String,
+    dateYear: Number,
+    startTime: String,
+    endTime: String,
+    location: String,
+    description: String,
+    cost: String,
+    image: String,
+    user: String,
+})
+const Events = mongoose.model("Events", EventsSchema)
+
+////
+// Middleware
+////
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
+////
+// Routes
+////
+app.get("/", (req, res) => {
+    res.send("Welcome to the Event API home route")
+})
+// Index
+app.get("/events", async (req, res) => {
+    try {
+        res.json(await Events.find({}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+// New
+app.post("/events", async (req, res) => {
+    try {
+        res.json(await Events.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
+// Update
+app.put("/events/:id", async (req, res) => {
+    try {
+        res.json(
+            await Events.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        );
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+// Delete
+app.delete("/events/:id", async (req, res) => {
+    try {
+        res.json(await Events.findByIdAndRemove(req.params.id));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+////
+// Listener
+////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
